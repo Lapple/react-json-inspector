63fef9a14c87e28140d203674956ef9654fe331d
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2014-10-16 16:33:30 +0200

    Debounce SearchBar change callback

--- lib/search-bar.jsx
+++ lib/search-bar.jsx
@@ -1,16 +1,17 @@
 var React = require('react');
+var debounce = require('debounce');
 
 var SearchBar = React.createClass({
     getDefaultProps: function() {
         return {
             minlength: 2,
+            timeout: 100,
             onChange: function() {}
         };
     },
     render: function() {
-        return <input type='text' placeholder='Search' ref='query' onInput={ this.update } />;
+        return <input type='text' placeholder='Search' ref='query' onInput={ debounce(this.update, this.props.timeout) } />;
     },
-    // TODO: Throttle
     update: function() {
         var value = this.refs.query.getDOMNode().value;
 
