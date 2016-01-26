9b4ed9c65b1afd67a96e8066958061d3ad394f01
Aviad Lichtenstadt <aviad@demisto.com>
2016-01-26 15:55:33 +0200

    Added option to inject filterer Added option to inject highlighter condition

--- json-inspector.js
+++ json-inspector.js
@@ -23,7 +23,9 @@ module.exports = React.createClass({
         validateQuery: React.PropTypes.func,
         isExpanded: React.PropTypes.func,
         filterOptions: React.PropTypes.object,
-        query: React.PropTypes.string
+        query: React.PropTypes.string,
+        filterFunc: React.PropTypes.func,
+        shouldHighlight: React.PropTypes.func
     },
 
     getDefaultProps: function() {
@@ -59,42 +61,43 @@ module.exports = React.createClass({
         var s = this.state;
 
         var isQueryValid = (
-            s.query !== '' &&
-            p.validateQuery(s.query)
+          s.query !== '' &&
+          p.validateQuery(s.query)
         );
 
         var data = (
-            isQueryValid ?
-                s.filterer(s.query) :
-                p.data
+          isQueryValid ?
+            s.filterer(s.query) :
+            p.data
         );
 
         var isNotFound = (
-            isQueryValid &&
-            isEmpty(data)
+          isQueryValid &&
+          isEmpty(data)
         );
 
         return D.div({ className: 'json-inspector ' + p.className },
-            this.renderToolbar(),
-            (
-                isNotFound ?
-                    D.div({ className: 'json-inspector__not-found' }, 'Nothing found') :
-                    leaf({
-                        data: data,
-                        onClick: p.onClick,
-                        id: p.id,
-                        getOriginal: this.getOriginal,
-                        query: (
-                            isQueryValid ?
-                                s.query :
-                                null
-                        ),
-                        label: 'root',
-                        root: true,
-                        isExpanded: p.isExpanded,
-                        interactiveLabel: p.interactiveLabel
-                    })
-            )
+          this.renderToolbar(),
+          (
+            isNotFound ?
+              D.div({ className: 'json-inspector__not-found' }, 'Nothing found') :
+              leaf({
+                  data: data,
+                  onClick: p.onClick,
+                  id: p.id,
+                  getOriginal: this.getOriginal,
+                  query: (
+                    isQueryValid ?
+                      s.query :
+                      null
+                  ),
+                  label: 'root',
+                  root: true,
+                  isExpanded: p.isExpanded,
+                  interactiveLabel: p.interactiveLabel,
+                  shouldHighlight: p.shouldHighlight,
+              })
+          )
         );
     },
     renderToolbar: function() {
@@ -102,11 +105,11 @@ module.exports = React.createClass({
 
         if (search) {
             return D.div({ className: 'json-inspector__toolbar' },
-                search({
-                    onChange: this.search,
-                    data: this.props.data,
-                    query: this.state.query
-                })
+              search({
+                  onChange: this.search,
+                  data: this.props.data,
+                  query: this.state.query
+              })
             );
         }
     },
@@ -116,21 +119,30 @@ module.exports = React.createClass({
         });
     },
     componentWillMount: function() {
-        this.createFilterer(this.props.data, this.props.filterOptions);
+        this.createFilterer(this.props.data, this.props.filterOptions, this.props.filterFunc);
     },
     componentWillReceiveProps: function(p) {
-        this.createFilterer(p.data, p.filterOptions);
+        this.createFilterer(p.data, p.filterOptions, p.filterFunc);
     },
     shouldComponentUpdate: function (p, s) {
         return (
-            s.query !== this.state.query ||
-            p.data !== this.props.data ||
-            p.onClick !== this.props.onClick
+          p.filterFunc !== this.props.filterFunc ||
+          s.query !== this.state.query ||
+          p.data !== this.props.data ||
+          p.onClick !== this.props.onClick
         );
     },
-    createFilterer: function(data, options) {
+    getFilterer: function(pFilterFunc) {
+        var filterFunc = pFilterFunc;
+        if (!filterFunc) {
+            filterFunc = filterer
+        }
+        return filterFunc;
+    },
+    createFilterer: function(data, options, filterFunc) {
+        var f = this.getFilterer(filterFunc);
         this.setState({
-            filterer: filterer(data, options)
+            filterer: f(data, options)
         });
     },
     getOriginal: function(path) {
