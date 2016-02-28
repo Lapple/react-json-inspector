3a9731165a49e6b642089f2b3dd38f64d1b829c2
Aviad Lichtenstadt <aviad@demisto.com>
2016-02-28 02:52:18 +0200

    fix version

--- json-inspector.js
+++ json-inspector.js
@@ -12,146 +12,141 @@ var lens = require('./lib/lens');
 var noop = require('./lib/noop');
 
 module.exports = React.createClass({
-    propTypes: {
-        data: React.PropTypes.any.isRequired,
-        // For now it expects a factory function, not element.
-        search: React.PropTypes.oneOfType([
-            React.PropTypes.func,
-            React.PropTypes.bool
-        ]),
-        onClick: React.PropTypes.func,
-        validateQuery: React.PropTypes.func,
-        isExpanded: React.PropTypes.func,
-        isRootCollapsed: React.PropTypes.bool,
-        filterOptions: React.PropTypes.object,
-        query: React.PropTypes.string,
-        filterFunc: React.PropTypes.func,
-        shouldHighlight: React.PropTypes.func,
-        highlightRenderer: React.PropTypes.func
-    },
+  propTypes: {
+    data: React.PropTypes.any.isRequired,
+    // For now it expects a factory function, not element.
+    search: React.PropTypes.oneOfType([
+      React.PropTypes.func,
+      React.PropTypes.bool
+    ]),
+    onClick: React.PropTypes.func,
+    validateQuery: React.PropTypes.func,
+    isExpanded: React.PropTypes.func,
+    isRootCollapsed: React.PropTypes.bool,
+    filterOptions: React.PropTypes.object,
+    query: React.PropTypes.string,
+    filterFunc: React.PropTypes.func,
+    shouldHighlight: React.PropTypes.func,
+    highlightRenderer: React.PropTypes.func
+  },
 
-    getDefaultProps: function() {
-        return {
-            data: null,
-            search: searchBar,
-            className: '',
-            id: 'json-' + Date.now(),
-            onClick: noop,
-            filterOptions: {},
-            validateQuery: function(query) {
-                return query.length >= 2;
-            },
-            /**
-             * Decide whether the leaf node at given `keypath` should be
-             * expanded initially.
-             * @param  {String} keypath
-             * @param  {Any} value
-             * @return {Boolean}
-             */
-            isExpanded: function(keypath, value) {
-                return false;
-            },
-            highlightRenderer: null
-        };
-    },
-    getInitialState: function() {
-        return {
-            query: this.props.query || ''
-        };
-    },
-    render: function() {
-        var p = this.props;
-        var s = this.state;
+  getDefaultProps: function() {
+    return {
+      data: null,
+      search: searchBar,
+      className: '',
+      id: 'json-' + Date.now(),
+      onClick: noop,
+      filterOptions: {},
+      validateQuery: function(query) {
+        return query !== null && query !== undefined;
+      },
+      /**
+       * Decide whether the leaf node at given `keypath` should be
+       * expanded initially.
+       * @param  {String} keypath
+       * @param  {Any} value
+       * @return {Boolean}
+       */
+      isExpanded: function(keypath, value) {
+        return false;
+      },
+      highlightRenderer: null
+    };
+  },
+  render: function() {
+    var p = this.props;
+    var s = this.state;
 
-        var isQueryValid = (
-          s.query !== '' &&
-          p.validateQuery(s.query)
-        );
+    var isQueryValid = (
+      p.query !== '' &&
+      p.validateQuery(p.query)
+    );
 
-        var data = (
-          isQueryValid ?
-            s.filterer(s.query) :
-            p.data
-        );
+    var data = (
+      isQueryValid ?
+        s.filterer(p.query) :
+        p.data
+    );
 
-        var isNotFound = (
-          isQueryValid &&
-          isEmpty(data)
-        );
+    var isNotFound = (
+      isQueryValid &&
+      isEmpty(data)
+    );
 
-        return D.div({ className: 'json-inspector ' + p.className },
-          this.renderToolbar(),
-          (
-            isNotFound ?
-              D.div({ className: 'json-inspector__not-found' }, 'Nothing found') :
-              leaf({
-                  data: data,
-                  onClick: p.onClick,
-                  id: p.id,
-                  getOriginal: this.getOriginal,
-                  query: (
-                    isQueryValid ?
-                      s.query :
-                      null
-                  ),
-                  label: 'root',
-                  root: true,
-                  isRootCollapsed: p.isRootCollapsed,
-                  isExpanded: p.isExpanded,
-                  interactiveLabel: p.interactiveLabel,
-                  shouldHighlight: p.shouldHighlight,
-                  highlightRenderer: p.highlightRenderer,
-              })
-          )
-        );
-    },
-    renderToolbar: function() {
-        var search = this.props.search;
+    return D.div({ className: 'json-inspector ' + p.className },
+      this.renderToolbar(),
+      (
+        isNotFound ?
+          D.div({ className: 'json-inspector__not-found' }, 'Nothing found') :
+          leaf({
+            data: data,
+            onClick: p.onClick,
+            id: p.id,
+            getOriginal: this.getOriginal,
+            query: (
+              isQueryValid ?
+                p.query :
+                null
+            ),
+            label: 'root',
+            root: true,
+            isRootCollapsed: p.isRootCollapsed,
+            isExpanded: p.isExpanded,
+            interactiveLabel: p.interactiveLabel,
+            shouldHighlight: p.shouldHighlight,
+            highlightRenderer: p.highlightRenderer,
+          })
+      )
+    );
+  },
+  renderToolbar: function() {
+    var search = this.props.search;
 
-        if (search) {
-            return D.div({ className: 'json-inspector__toolbar' },
-              search({
-                  onChange: this.search,
-                  data: this.props.data,
-                  query: this.state.query
-              })
-            );
-        }
-    },
-    search: function(query) {
-        this.setState({
-            query: query
-        });
-    },
-    componentWillMount: function() {
-        this.createFilterer(this.props.data, this.props.filterOptions, this.props.filterFunc);
-    },
-    componentWillReceiveProps: function(p) {
-        this.createFilterer(p.data, p.filterOptions, p.filterFunc);
-        this.setState({query: p.query});
-    },
-    shouldComponentUpdate: function (p, s) {
-        return (
-          p.filterFunc !== this.props.filterFunc ||
-          s.query !== this.state.query ||
-          p.data !== this.props.data ||
-          p.onClick !== this.props.onClick
-        );
-    },
-    getFilterer: function(pFilterFunc) {
-        var filterFunc = pFilterFunc;
-        if (!filterFunc) {
-            filterFunc = filterer
-        }
-        return filterFunc;
-    },
-    createFilterer: function(data, options, filterFunc) {
-        var f = this.getFilterer(filterFunc);
-        this.setState({
-            filterer: f(data, options)
-        });
-    },
-    getOriginal: function(path) {
-        return lens(this.props.data, path);
+    if (search) {
+      return D.div({ className: 'json-inspector__toolbar' },
+        search({
+          onChange: this.search,
+          data: this.props.data,
+          query: this.state.query
+        })
+      );
     }
+  },
+  search: function(query) {
+    this.setState({
+      query: query
+    });
+  },
+  componentWillMount: function() {
+    this.createFilterer(this.props.data, this.props.filterOptions, this.props.filterFunc);
+  },
+  componentWillReceiveProps: function(p) {
+    this.createFilterer(p.data, p.filterOptions, p.filterFunc);
+    this.setState({query: p.query});
+  },
+  shouldComponentUpdate: function (p, s) {
+    return (
+      p.filterFunc !== this.props.filterFunc ||
+      p.query !== this.props.query ||
+      p.data !== this.props.data ||
+      p.onClick !== this.props.onClick
+    );
+  },
+  getFilterer: function(pFilterFunc) {
+    var filterFunc = pFilterFunc;
+    if (!filterFunc) {
+      filterFunc = filterer
+    }
+    return filterFunc;
+  },
+  createFilterer: function(data, options, filterFunc) {
+    var f = this.getFilterer(filterFunc);
+    this.setState({
+      filterer: f(data, options)
+    });
+  },
+  getOriginal: function(path) {
+    return lens(this.props.data, path);
+  }
 });
