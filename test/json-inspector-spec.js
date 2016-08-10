ae443b9ccd4daec9faad563947bf6f2fc633563b
Aziz Yuldoshev <yuldoshev.aziz@gmail.com>
2016-08-10 08:11:25 +0200

    Add tests for root node

--- test/json-inspector-spec.js
+++ test/json-inspector-spec.js
@@ -32,7 +32,17 @@ describe('<Inspector />', () => {
         expect(this.root.state('expanded')).to.equal(true);
     });
 
-    it('has 46 child nodes', function() {
-        expect(this.root.find(Leaf)).to.have.length(46);
+    describe('root leaf', function() {
+        it('has 46 child nodes', function() {
+            expect(this.root.find(Leaf)).to.have.length(46);
+        });
+
+        it('has all his child nodes collapsed', function() {
+            expect(this.root.find(Leaf).everyWhere(l => !l.shallow().state('expanded'))).to.equal(true);
+        });
+
+        it('has no nested root nodes', function() {
+            expect(this.root.find(Leaf).everyWhere(l => !l.shallow().prop('root'))).to.equal(true);
+        });
     });
 });
